aedc2b86ca47231b26abbb0a264e5b9e27a08173
Geoff Davis <gadavis@ucsd.edu>
2011-05-19 16:28:42 -0700

    Add full paths to installed modules in php tests

--- t/php/antelope-modules.php
+++ t/php/antelope-modules.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
-dl( 'Orb.so' ) or die( 'Cannot load Orb shared object' );
-dl( 'Datascope.so' ) or die ( 'Cannot load Datascope shared object' );
+$antelope=getenv('ANTELOPE');
+dl( $antelope . '/local/data/php/' . 'Orb.so' ) or die( 'Cannot load Orb shared object' );
+dl( $antelope . '/local/data/php/' . 'Datascope.so' ) or die ( 'Cannot load Datascope shared object' );
 ?>
--- t/php/pdflib-module.php
+++ t/php/pdflib-module.php
@@ -1,4 +1,4 @@
 #!/usr/bin/env php
 <?php
-dl( 'libpdf_php.so' ) or die( "Can't load PDFLite shared object." );
+dl( '/opt/anf/PDFlib-7.0.4p1-SunOS-sparc-php/bind/php5/php-520/libpdf_php.so' ) or die( "Can't load PDFLite shared object." );
 ?>

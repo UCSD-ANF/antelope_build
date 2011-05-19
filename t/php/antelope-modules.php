06be6805d7c4b5e9ab72680d015d815558c5f1e3
Geoff Davis <gadavis@ucsd.edu>
2011-05-19 16:16:59 -0700

    Add rudimentary php tests

---
+++ t/php/antelope-modules.php
@@ -0,0 +1,5 @@
+#!/usr/bin/env php
+<?php
+dl( 'Orb.so' ) or die( 'Cannot load Orb shared object' );
+dl( 'Datascope.so' ) or die ( 'Cannot load Datascope shared object' );
+?>
---
+++ t/php/pdflib-module.php
@@ -0,0 +1,4 @@
+#!/usr/bin/env php
+<?php
+dl( 'libpdf_php.so' ) or die( "Can't load PDFLite shared object." );
+?>

95f1a15c4491867e8c71d35a8a7a5cda64a357b7
Geoff Davis <gadavis@ucsd.edu>
2011-10-10 16:50:25 -0700

    Update PHP tests to work without dl

--- t/php/antelope-modules.php
+++ t/php/antelope-modules.php
@@ -1,6 +1,11 @@
 #!/usr/bin/env php
 <?php
-$antelope=getenv('ANTELOPE');
-dl( $antelope . '/local/data/php/' . 'Orb.so' ) or die( 'Cannot load Orb shared object' );
-dl( $antelope . '/local/data/php/' . 'Datascope.so' ) or die ( 'Cannot load Datascope shared object' );
+if( !extension_loaded( 'Datascope') ) {
+  die "Antelope Datascope extension is not loaded.";
+}
+
+if( !extension_loaded( 'Orb') ) {
+  die "Antelope Orb extension is not loaded.";
+}
+echo "Antelope extensions are loaded properly";
 ?>
--- t/php/pdflib-module.php
+++ t/php/pdflib-module.php
@@ -1,4 +1,6 @@
 #!/usr/bin/env php
 <?php
-dl( '/opt/anf/PDFlib-7.0.4p1-SunOS-sparc-php/bind/php5/php-520/libpdf_php.so' ) or die( "Can't load PDFLite shared object." );
+if( !extension_loaded( 'pdf' ) ) {
+  die ("PDFLib extension is not loaded.");
+}
 ?>

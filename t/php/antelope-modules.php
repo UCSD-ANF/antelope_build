5cba5e5115bae2bed1128180e2241af85d69dc6e
Geoff Davis <gadavis@ucsd.edu>
2015-05-11 05:31:22 +0000

    Add tests for php module compilation

--- t/php/antelope-modules.php
+++ t/php/antelope-modules.php
@@ -1,11 +1,15 @@
 #!/usr/bin/env php
 <?php
-if( !extension_loaded( 'Datascope') ) {
-  die "Antelope Datascope extension is not loaded.";
+$extensions = array('Datascope','Orb','sysinfo');
+foreach($extensions as $extension) {
+	if( !extension_loaded( $extension ) ) {
+		print ("Antelope $extension extension is not loaded.");
+		exit (1);
+	}
+	$functions = get_extension_funcs($extension);
+	echo "Functions available in the $extension extension:<br>\n";
+	foreach($functions as $func) {
+		echo $func."<br>\n";}
 }
-
-if( !extension_loaded( 'Orb') ) {
-  die "Antelope Orb extension is not loaded.";
-}
-echo "Antelope extensions are loaded properly";
+echo "Antelope extensions are loaded properly.<br>\n";
 ?>
